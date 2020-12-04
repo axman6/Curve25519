6d1a1fdb2b5a72dce1c539ede330cb746d7c0f0d
Alex Mason <Axman6@gmail.com>
2020-12-04 14:15:04 +1100

    Print inverse test in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Curve25519 ()
+import Curve25519 (testInverseMul, unpack25519)
+import Data.ByteString.Base16
 
 
 main :: IO ()
-main = pure ()
+main = print . ("0x" <>) . encode =<< testInverseMul (unpack25519 "0123456789abcdef0123456789abcdef")
